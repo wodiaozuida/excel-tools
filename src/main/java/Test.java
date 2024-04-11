94f1f0588e74866627787c7b5a44b4eb390bcf15
Rex <497907400@qq.com>
2024-04-11 12:30:36 +0800

    Fix douyin url

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -7,6 +7,8 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Test {
     public static void main(String[] args) {
@@ -54,7 +56,7 @@ public class Test {
 //                return parts[0];
                 return douyinUrl2(parts[0]);
             }else if(responseCode == 200){
-                return douyinUrl(url);
+                return douyinAllUrl(url);
             }
 
             connection.disconnect();
@@ -77,6 +79,31 @@ public class Test {
         String replacement = "https://www.douyin.com/video/$1";
 
         return input.replaceAll(pattern, replacement);
+    }
+
+    public static String douyinAllUrl(String input){
+//        String pattern1 = "https://www.douyin.com/user/[^/]+\\?modal_id=(\\d+)&.*";
+        String pattern1 = "https://www.douyin.com/user/[^/]+\\?modal_id=(\\d+)";
+        String pattern2 = "https://www.iesdouyin.com/share/video/(\\d+)/";
+
+        // 创建Pattern对象
+        Pattern p1 = Pattern.compile(pattern1);
+        Pattern p2 = Pattern.compile(pattern2);
+
+        // 创建Matcher对象
+        Matcher m1 = p1.matcher(input);
+        Matcher m2 = p2.matcher(input);
+
+        if (m1.matches()) {
+            return "https://www.douyin.com/video/" + m1.group(1);
+        }
+
+        // 如果匹配第二个模式，则执行替换
+        if (m2.matches()) {
+            return "https://www.douyin.com/video/" + m2.group(1);
+        }
+
+        return "";
     }
 
 }
